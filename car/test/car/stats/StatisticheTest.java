0f1a6257bd4e0b39bfce2d10a79c07858c39155f
anton <anton@192.168.1.59>
2020-02-13 00:17:55 +0100

    test terza domanda

--- car/test/car/stats/StatisticheTest.java
+++ car/test/car/stats/StatisticheTest.java
@@ -1,9 +1,20 @@
 package car.stats;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.Image;
+import java.util.Collections;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import car.auto.Auto;
+import car.auto.Bianca;
+import car.sim.Coordinate;
+import car.sim.Tragitto;
+import car.sim.Zona;
 
 public class StatisticheTest {
 
@@ -15,9 +26,21 @@ public class StatisticheTest {
 	}
 	
 	@Test
-	public void testTragittoPerAuto() {
+	public void testTragittoPerAutoVuoto() {
 		// DA COMPLETARE ( VEDI DOMANDA 3 )
-		fail("DA COMPLETARE");
+		
+		assertTrue(this.stats.tragittoPerAuto(Collections.emptyList()).isEmpty());
+	}
+	
+	
+	@Test
+	public void TestAutoTragitto() {
+		Auto bianca = new Bianca(new Zona());
+		Tragitto t = new Tragitto(bianca, new Coordinate(0, 0), new Coordinate(3, 2));
+		
+		assertTrue(this.stats.tragittoPerAuto(Collections.singletonList(t)).size()==1);
+		assertTrue(this.stats.tragittoPerAuto(Collections.singletonList(t)).containsKey(bianca));
+		
 	}
 
 }
