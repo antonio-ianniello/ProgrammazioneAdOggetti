6d5de170c3ebfc1d8da8021adf22664c1d7ee4b4
anton <anton@192.168.1.59>
2020-02-13 15:35:58 +0100

    Aggiunto progetto gen

---
+++ gen/test/gen/stats/StatisticheTest.java
@@ -0,0 +1,26 @@
+package gen.stats;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gen.stats.Statistiche;
+
+public class StatisticheTest {
+
+	@SuppressWarnings("unused")
+	private Statistiche stats;
+
+	@Before
+	public void setUp() {
+		this.stats = new Statistiche();
+	}
+	
+	@Test
+	public void testScontriPerAnimale() {
+		// DA COMPLETARE ( VEDI DOMANDA 3 )
+		fail("DA COMPLETARE");
+	}
+
+}
